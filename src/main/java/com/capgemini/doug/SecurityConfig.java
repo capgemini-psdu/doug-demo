e31c87b554ef7a0b8bd9cb2d248c593bdebfd8a3
James Devaney <james.devaney@capgemini.com>
2017-04-27 12:32:54 +0100

    Remove security

--- src/main/java/com/capgemini/doug/SecurityConfig.java
+++ src/main/java/com/capgemini/doug/SecurityConfig.java
@@ -1,40 +1,37 @@
 package com.capgemini.doug;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configurers.GlobalAuthenticationConfigurerAdapter;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 @Order(Ordered.HIGHEST_PRECEDENCE)
 @Configuration
-@EnableGlobalMethodSecurity(securedEnabled = true)
+//@EnableGlobalMethodSecurity(securedEnabled = true)
 public class SecurityConfig extends GlobalAuthenticationConfigurerAdapter {
 
-	private static final String ROLE_USER = "USER";
-
-	private static final String ROLE_ADMIN = "ADMIN";
-
-	@Value("${appsecurity.admin.username}")
-	private String adminUsername;
-
-	@Value("${appsecurity.admin.password}")
-	private String adminPassword;
-
-	@Value("${appsecurity.user.username}")
-	private String userUsername;
-
-	@Value("${appsecurity.user.password}")
-	private String userPassword;
-
-	@Override
-	public void init(AuthenticationManagerBuilder auth) throws Exception {
-		
-		auth.inMemoryAuthentication()
-			.withUser(adminUsername).password(adminPassword).roles(ROLE_ADMIN)
-			.and()
-			.withUser(userUsername).password(userPassword).roles(ROLE_USER);
-	}
+//	private static final String ROLE_USER = "USER";
+//
+//	private static final String ROLE_ADMIN = "ADMIN";
+//
+//	@Value("${appsecurity.admin.username}")
+//	private String adminUsername;
+//
+//	@Value("${appsecurity.admin.password}")
+//	private String adminPassword;
+//
+//	@Value("${appsecurity.user.username}")
+//	private String userUsername;
+//
+//	@Value("${appsecurity.user.password}")
+//	private String userPassword;
+//
+//	@Override
+//	public void init(AuthenticationManagerBuilder auth) throws Exception {
+//		
+//		auth.inMemoryAuthentication()
+//			.withUser(adminUsername).password(adminPassword).roles(ROLE_ADMIN)
+//			.and()
+//			.withUser(userUsername).password(userPassword).roles(ROLE_USER);
+//	}
 }
